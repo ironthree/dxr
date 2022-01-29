9074fab166ebb0bf6f1c4469ae58e7c7a2e6ca86
Fabio Valentini <decathorpe@gmail.com>
2022-01-29 16:14:49 +0100

    xmlrpc: add more tests for ser/de types and values

--- src/tests.rs
+++ src/tests.rs
@@ -1,10 +1,34 @@
 use chrono::{SubsecRound, Utc};
 
 use crate::XML_RPC_DATE_FORMAT;
-use crate::{Array, Member, Struct, Value};
+use crate::{Array, Member, Struct, Type, Value};
 
 //use serde_xml_rs::{from_str, to_string};
 use quick_xml::{de::from_str, se::to_string};
+
+#[test]
+fn to_i4() {
+    let value = Type::Integer(-12);
+    let expected = "<i4>-12</i4>";
+
+    assert_eq!(to_string(&value).unwrap(), expected);
+}
+
+#[test]
+fn from_i4() {
+    let value = "<i4>-12</i4>";
+    let expected = Type::Integer(-12);
+
+    assert_eq!(from_str::<Type>(value).unwrap(), expected);
+}
+
+#[test]
+fn from_int() {
+    let value = "<int>-12</int>";
+    let expected = Type::Integer(-12);
+
+    assert_eq!(from_str::<Type>(value).unwrap(), expected);
+}
 
 #[test]
 fn to_value_i4() {
@@ -32,6 +56,24 @@ fn from_value_int() {
 
 #[cfg(feature = "i8")]
 #[test]
+fn to_i8() {
+    let value = Type::Long(-12);
+    let expected = "<i8>-12</i8>";
+
+    assert_eq!(to_string(&value).unwrap(), expected);
+}
+
+#[cfg(feature = "i8")]
+#[test]
+fn from_i8() {
+    let value = "<i8>-12</i8>";
+    let expected = Type::Long(-12);
+
+    assert_eq!(from_str::<Type>(value).unwrap(), expected);
+}
+
+#[cfg(feature = "i8")]
+#[test]
 fn to_value_i8() {
     let value = Value::i8(-12);
     let expected = "<value><i8>-12</i8></value>";
@@ -46,6 +88,22 @@ fn from_value_i8() {
     let expected = Value::i8(-12);
 
     assert_eq!(from_str::<Value>(value).unwrap(), expected);
+}
+
+#[test]
+fn to_boolean() {
+    let value = Type::Boolean(false);
+    let expected = "<boolean>0</boolean>";
+
+    assert_eq!(to_string(&value).unwrap(), expected);
+}
+
+#[test]
+fn from_boolean() {
+    let value = "<boolean>0</boolean>";
+    let expected = Type::Boolean(false);
+
+    assert_eq!(from_str::<Type>(value).unwrap(), expected);
 }
 
 #[test]
@@ -65,6 +123,22 @@ fn from_value_boolean() {
 }
 
 #[test]
+fn to_str() {
+    let value = Type::String(String::from("Hello, World!"));
+    let expected = "<string>Hello, World!</string>";
+
+    assert_eq!(to_string(&value).unwrap(), expected);
+}
+
+#[test]
+fn from_string() {
+    let value = "<string>Hello, World!</string>";
+    let expected = Type::String(String::from("Hello, World!"));
+
+    assert_eq!(from_str::<Type>(value).unwrap(), expected);
+}
+
+#[test]
 fn to_value_string() {
     let value = Value::string(String::from("Hello, World!"));
     let expected = "<value><string>Hello, World!</string></value>";
@@ -81,6 +155,22 @@ fn from_value_string() {
 }
 
 #[test]
+fn to_double() {
+    let value = Type::Double(1.5);
+    let expected = "<double>1.5</double>";
+
+    assert_eq!(to_string(&value).unwrap(), expected);
+}
+
+#[test]
+fn from_double() {
+    let value = "<double>1.5</double>";
+    let expected = Type::Double(1.5);
+
+    assert_eq!(from_str::<Type>(value).unwrap(), expected);
+}
+
+#[test]
 fn to_value_double() {
     let value = Value::double(1.5);
     let expected = "<value><double>1.5</double></value>";
@@ -94,6 +184,28 @@ fn from_value_double() {
     let expected = Value::double(1.5);
 
     assert_eq!(from_str::<Value>(value).unwrap(), expected);
+}
+
+#[test]
+fn to_datetime() {
+    let datetime = Utc::now();
+    let datetime_str = datetime.format(XML_RPC_DATE_FORMAT).to_string();
+
+    let value = Type::DateTime(datetime);
+    let expected = format!("<dateTime.iso8601>{}</dateTime.iso8601>", datetime_str);
+
+    assert_eq!(to_string(&value).unwrap(), expected);
+}
+
+#[test]
+fn from_datetime() {
+    let datetime = Utc::now().round_subsecs(0);
+    let datetime_str = datetime.format(XML_RPC_DATE_FORMAT).to_string();
+
+    let value = format!("<dateTime.iso8601>{}</dateTime.iso8601>", datetime_str);
+    let expected = Type::DateTime(datetime);
+
+    assert_eq!(from_str::<Type>(&value).unwrap(), expected);
 }
 
 #[test]
@@ -116,6 +228,28 @@ fn from_value_datetime() {
     let expected = Value::datetime(datetime);
 
     assert_eq!(from_str::<Value>(&value).unwrap(), expected);
+}
+
+#[test]
+fn to_base64() {
+    let contents = b"you can't read this!";
+    let encoded = base64::encode(contents);
+
+    let value = Type::Base64(contents.to_vec());
+    let expected = format!("<base64>{}</base64>", encoded);
+
+    assert_eq!(to_string(&value).unwrap(), expected);
+}
+
+#[test]
+fn from_base64() {
+    let contents = b"you can't read this!";
+    let encoded = base64::encode(contents);
+
+    let value = format!("<base64>{}</base64>", encoded);
+    let expected = Type::Base64(contents.to_vec());
+
+    assert_eq!(from_str::<Type>(&value).unwrap(), expected);
 }
 
 #[test]
@@ -311,6 +445,24 @@ fn from_value_array() {
     let expected = Value::array(Array::from_elements(vec![Value::i4(-12)]));
 
     assert_eq!(from_str::<Value>(value).unwrap(), expected);
+}
+
+#[cfg(feature = "nil")]
+#[test]
+fn to_nil() {
+    let value = Type::Nil;
+    let expected = "<nil/>";
+
+    assert_eq!(to_string(&value).unwrap(), expected);
+}
+
+#[cfg(feature = "nil")]
+#[test]
+fn from_nil() {
+    let value = "<nil/>";
+    let expected = Type::Nil;
+
+    assert_eq!(from_str::<Type>(value).unwrap(), expected);
 }
 
 #[cfg(feature = "nil")]
