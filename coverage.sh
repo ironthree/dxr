6ed11a5ee6bbd789d508abd6a1333d88a616eafc
Fabio Valentini <decathorpe@gmail.com>
2023-07-05 15:54:15 +0200

    add script for running tests with coverage

---
+++ coverage.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/bash
+
+set -e
+
+export RUSTFLAGS="-Cinstrument-coverage"
+cargo build --workspace --all-features
+export LLVM_PROFILE_FILE="dxr-%p-%m.profraw"
+cargo test --workspace --all-features
+grcov . -s . --binary-path ./target/debug/ -t html --branch --ignore-not-existing -o ./target/debug/coverage/
+rm **/*.profraw
+
