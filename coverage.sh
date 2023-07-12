2febaeb84e0a5b603a80283ae5ca2646eac9f60a
Fabio Valentini <decathorpe@gmail.com>
2023-07-12 12:22:58 +0200

    coverage: ensure removal of all temporariy files

--- coverage.sh
+++ coverage.sh
@@ -7,5 +7,5 @@ cargo build --workspace --all-features
 export LLVM_PROFILE_FILE="dxr-%p-%m.profraw"
 cargo test --workspace --all-features
 grcov . -s . --binary-path ./target/debug/ -t html --branch --ignore-not-existing -o ./target/debug/coverage/
-rm **/*.profraw
+rm *.profraw **/*.profraw
 
